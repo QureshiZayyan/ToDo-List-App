98381e473e3c9ddd88d10ee6f60cb65f5ea0d9a1
zayyanhere <zayyanq01@gmail.com>
2024-08-01 19:20:37 +0530

    added changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,10 @@
 import './App.css';
-import { useState } from 'react';
+import Todo from './components/Todo';
 
 const App = () => {
-  const [text, setText] = useState('');
-  const [input, setInputtext] = useState('');
-
-  const change = (e) => {
-    setInputtext(e.target.value);
-  }
-
-  const submit = (e) => {
-    e.preventDefault();
-    setText(input);
-    setInputtext('');
-  }
-
   return (
     <>
-      <div className="container">
-        <div className="heading">
-          <h2>My ToDo List</h2>
-
-        </div>
-
-        <div className="main">
-          <form onSubmit={submit}>
-            <input type="text" id="name" name="name" className="list" onChange={change} value={input} />
-            <button className="btn">Add List</button>
-          </form>
-        </div>
-
-        <div className="listvalue">
-          <p>{text}</p>
-        </div>
-      </div >
+      <Todo />
     </>
   )
 }
---
+++ src/components/Todo.jsx
@@ -0,0 +1,49 @@
+import React from 'react'
+import { useState } from 'react';
+import { MdDelete } from "react-icons/md";
+import { FaCheckCircle } from "react-icons/fa";
+
+const Todo = () => {
+  const [todo, setTodo] = useState('');
+  const [todolist, setTodoList] = useState([]);
+  const [input, setInput] = useState('');
+
+  const submit = (e) => {
+    e.preventDefault();
+    if (!input) return;
+    setTodo(input);
+    setTodoList([...todolist, todo])
+    setInput('');
+  }
+
+  return (
+    <>
+    <MdDelete className='icons' size={40}/>
+    <FaCheckCircle className='icons' size={40}/>
+      <div className="container">
+        <div className="heading">
+          <h2>My ToDo List</h2>
+        </div>
+        <div className="main">
+          <form onSubmit={submit}>
+            <input type="text" id="name" name="name" className="list" value={input} onChange={(e) => setInput(e.target.value)} />
+            <button className="btn">Add List</button>
+          </form>
+        </div>
+
+        <div className="listvalue">
+          <ul>
+            {todolist.map((todo, index) => (
+              <li key={index}>
+                {todo}
+                <button onClick={() => removeTodo(index)}>Remove</button>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div >
+    </>
+  )
+}
+
+export default Todo;
